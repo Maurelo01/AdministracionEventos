ff8e2192dfee1bdfd677ecbb214187ca4ab1947c
maurelo860@gmail.com <maurelo860@gmail.com>
2025-08-12 22:51:45 -0600

    Proyecto base con main limpio

---
+++ administracionEventos/src/main/java/com/mycompany/administracioneventos/modelos/Participante.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.administracioneventos.modelos;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author mauricio
+ */
+public class Participante 
+{   
+    private String codigo;
+    private String correo;
+    private String institucion;
+    private LocalDate fecha;
+    private TipoParticipante tipo;        // enums
+    private String nombre;
+
+    public Participante()
+}
