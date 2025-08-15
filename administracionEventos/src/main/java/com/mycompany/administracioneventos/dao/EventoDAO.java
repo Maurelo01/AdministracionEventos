dd87ee2f6cceab208b6119fe706b5ebed8df46d9
maurelo860@gmail.com <maurelo860@gmail.com>
2025-08-14 21:18:45 -0600

    Agrega implementación de DAO para eventos

---
+++ administracionEventos/src/main/java/com/mycompany/administracioneventos/dao/EventoDAO.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.administracioneventos.dao;
+
+import com.mycompany.administracioneventos.modelos.Evento;
+import com.mycompany.administracioneventos.modelos.TipoEvento;
+import com.mycompany.administracioneventos.util.DBConnection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EventoDAO 
+{
+    public boolean agregarEvento(Evento evento) throws SQLException // Se encarga de crear un nuevo evento
+    {
+        String sql = "INSERT INTO evento (codigo, fecha, tipo, titulo, ubicacion, cupo_maximo) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) 
+        {
+            stmt.setString(1, evento.getCodigo());
+            stmt.setDate(2, Date.valueOf(evento.getFecha()));
+            stmt.setString(3, evento.getTipo().name());
+            stmt.setString(4, evento.getTitulo());
+            stmt.setString(5, evento.getUbicacion());
+            stmt.setInt(6, evento.getCupoMaximo());
+            return stmt.executeUpdate() > 0;
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al agregar evento: " + e.getMessage());
+            return false;
+        }
+    }
+    public List<Evento> listarEventos() // Se encarga de listar los eventos
+    {
+        List<Evento> lista = new ArrayList<>();
+        String sql = "SELECT * FROM evento";
+        try (Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql))
+        {
+            while (rs.next())
+            {
+                 Evento evento = new Evento
+                        (
+                                rs.getString("codigo"),
+                                rs.getDate("fecha").toLocalDate(),
+                                TipoEvento.valueOf(rs.getString("tipo")),
+                                rs.getString("titulo"),
+                                rs.getString("ubicacion"),
+                                rs.getInt("cupo_maximo")
+                        );
+                 lista.add(evento);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al listar eventos: " + e.getMessage());
+        }
+        return lista;
+    }
+    
+    public Evento buscarEvento(String codigo) // Se encarga de buscar un evento por su codigoS
+    {
+        String sql = "SELECT * FROM evento WHERE codigo = ?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql))
+        {
+             stmt.setString(1, codigo);
+             ResultSet rs = stmt.executeQuery();
+             if (rs.next())
+             {
+                 return new Evento
+                        (
+                            rs.getString("codigo"),
+                            rs.getDate("fecha").toLocalDate(),
+                            TipoEvento.valueOf(rs.getString("tipo")),
+                            rs.getString("titulo"),
+                            rs.getString("ubicacion"),
+                            rs.getInt("cupo_maximo")   
+                        );
+             }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al buscar evento: " + e.getMessage());
+        }
+        return null;
+    }
+    
+    public boolean actualizarEvento(Evento evento) // Se encarga de actualizar los eventos
+    {
+        String sql = "UPDATE evento SET fecha=?, tipo=?, titulo=?, ubicacion=?, cupo_maximo=? WHERE codigo=?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql))
+        {
+            stmt.setDate(1, Date.valueOf(evento.getFecha()));
+            stmt.setString(2, evento.getTipo().name());
+            stmt.setString(3, evento.getTitulo());
+            stmt.setString(4, evento.getUbicacion());
+            stmt.setInt(5, evento.getCupoMaximo());
+            stmt.setString(6, evento.getCodigo());
+            
+            return stmt.executeUpdate() > 0;
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al actualizar evento: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean eliminarEvento(String codigo)
+    {
+        String sql = "DELETE FROM evento WHERE codigo=?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql))
+        {
+            stmt.setString(1, codigo);
+            return stmt.executeUpdate() > 0;
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al eliminar evento: " + e.getMessage());
+            return false;   
+        }
+    }
+}
