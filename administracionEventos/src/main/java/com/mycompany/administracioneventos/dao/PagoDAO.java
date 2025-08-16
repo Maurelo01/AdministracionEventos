98e6fa12d93e778d2aab517fc8c8e448d9c34865
maurelo860@gmail.com <maurelo860@gmail.com>
2025-08-15 23:22:51 -0600

    Agregado el servicio de Pago y modificada la funcion PagoDAO

--- administracionEventos/src/main/java/com/mycompany/administracioneventos/dao/PagoDAO.java
+++ administracionEventos/src/main/java/com/mycompany/administracioneventos/dao/PagoDAO.java
@@ -4,15 +4,50 @@
  */
 package com.mycompany.administracioneventos.dao;
 
-import com.mycompany.administracioneventos.modelos.Pago;
+import com.mycompany.administracioneventos.modelos.*;
 import com.mycompany.administracioneventos.util.DBConnection;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PagoDAO 
 {
+    private boolean existeParticipante(String correoParticipante) // Verifica si existe participante
+    {
+        String sql = "SELECT 1 FROM participante WHERE correo = ? LIMIT 1";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql))
+        {
+            ps.setString(1, correoParticipante);
+            try (ResultSet rs = ps.executeQuery())
+            {
+                return rs.next();
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al verificar participante: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    private boolean existeEvento(String codigoEvento) // Verifica si existe el evento
+    {
+        String sql = "SELECT 1 FROM evento WHERE codigo = ? LIMIT 1";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql))
+        {
+            ps.setString(1, codigoEvento);
+            try (ResultSet rs = ps.executeQuery())
+            {
+                return rs.next();
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al verificar evento: " + e.getMessage());
+            return false;
+        }
+    }
+    
     public boolean existePago(String correoParticipante, String codigoEvento) // Revisa si existe el pago
     {
         String sql = "SELECT 1 FROM pago WHERE participante_correo = ? AND evento_codigo = ? LIMIT 1";
@@ -32,13 +67,48 @@ public class PagoDAO
         }
     }
     
-    public boolean registrarPago(Pago pago)
+    public boolean existePagoId(int id) // Revisa si existe el pago mediante el id
     {
+        String sql = "DELETE FROM pago WHERE id = ? LIMIT 1";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql))
+        {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery())
+            {
+                return rs.next();
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al verificar pago por id: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean registrarPago(Pago pago) // Registra un pago despues de hacer las nuevas verificaciones
+    {
+        if (pago.getMonto() <= 0)
+        {
+            System.err.println("El monto del pago debe ser mayor a 0.");
+            return false;
+        }
+        String correoParticipante = pago.getParticipante().getCorreo();
+        String codigoEvento = pago.getEvento().getCodigo();
+        if (!existeEvento(codigoEvento))
+        {
+            System.err.println("No existe el evento: " + codigoEvento);
+            return false;
+        }
+        if (!existeParticipante(correoParticipante))
+        {
+            System.err.println("No existe el participante: " + correoParticipante);
+            return false;
+        }
         String sql = "INSERT INTO pago (participante_correo, evento_codigo, metodo, monto) VALUES (?,?,?,?)";
         try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) 
         {
-            ps.setString(1, pago.getParticipante().getCorreo());
-            ps.setString(2, pago.getEvento().getCodigo());
+            ps.setString(1, correoParticipante);
+            ps.setString(2, codigoEvento);
             ps.setString(3, pago.getMetodo().name());
             ps.setDouble(4, pago.getMonto());
             return ps.executeUpdate() > 0;
@@ -49,4 +119,94 @@ public class PagoDAO
             return false;
         }
     }
+    
+    public List<Pago> listarPagos() // Lista los pagos de forma general
+    {
+        List<Pago> lista = new ArrayList<>();
+        String sql = "SELECT id, participante_correo, evento_codigo, metodo, monto, fecha_pago FROM pago";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery())
+        {
+            ParticipanteDAO pDAO = new ParticipanteDAO();
+            EventoDAO eDAO = new EventoDAO();
+            while (rs.next())
+            {
+                String correoParticipante = rs.getString("participante_correo");
+                String codigoEvento = rs.getString("evento_codigo");
+                Participante p = pDAO.buscarParticipante(correoParticipante);
+                Evento e = eDAO.buscarEvento(codigoEvento);
+                Pago pago = new Pago(p, e, MetodoPago.valueOf(rs.getString("metodo")), rs.getDouble("monto"));
+                lista.add(pago);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al listar pago: " + e.getMessage());
+        }
+        return lista;
+    }
+    
+    public List<Pago> listarPagosPorEventos(String codigoEvento)  // Lista los pagos dependiendo de los eventos
+    {
+        List<Pago> lista = new ArrayList<>();
+        String sql = "SELECT participante_correo, metodo, monto, fecha_pago FROM pago WHERE evento_codigo = ?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery())
+        {
+            ps.setString(1, codigoEvento);
+            ParticipanteDAO pDAO = new ParticipanteDAO();
+            Evento e = new EventoDAO().buscarEvento(codigoEvento);
+            while (rs.next())
+            {
+                String correoParticipante = rs.getString("participante_correo");
+                Participante p = pDAO.buscarParticipante(correoParticipante);
+                Pago pago = new Pago(p, e, MetodoPago.valueOf(rs.getString("metodo")), rs.getDouble("monto"));
+                lista.add(pago);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al listar pago por evento: " + e.getMessage());
+        }
+        return lista;
+    }
+    
+    public List<Pago> listarPagosPorParticipante(String correoParticipante) // Lista los pagos dependiendo de los participantes
+    {
+        List<Pago> lista = new ArrayList<>();
+        String sql = "SELECT evento_codigo, metodo, monto, fecha_pago FROM pago WHERE participante_correo = ?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery())
+        {
+            ps.setString(1, correoParticipante);
+            Participante p = new ParticipanteDAO().buscarParticipante(correoParticipante);
+            EventoDAO eDAO = new EventoDAO();
+            while (rs.next())
+            {
+                String codigoEvento = rs.getString("evento_codigo");
+                Evento e = eDAO.buscarEvento(codigoEvento);
+                Pago pago = new Pago(p, e, MetodoPago.valueOf(rs.getString("metodo")), rs.getDouble("monto"));
+                lista.add(pago);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al listar pago por participantes: " + e.getMessage());
+        }
+        return lista;
+    }
+    
+    public boolean eliminarPagoPorId(int id) // Elimina pagos por medio de id
+    {
+        String sql = "DELETE FROM pago WHERE id = ?";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql))
+        {
+            ps.setInt(1, id);
+            return ps.executeUpdate() > 0;
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error al eliminar pago: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    
 }
---
+++ administracionEventos/src/main/java/com/mycompany/administracioneventos/servicios/PagoServicio.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.administracioneventos.servicios;
+
+import com.mycompany.administracioneventos.dao.*;
+import com.mycompany.administracioneventos.modelos.*;
+import java.util.List;
+public class PagoServicio 
+{
+    private final PagoDAO pagoDAO;
+    private final ParticipanteDAO pDAO;
+    private final EventoDAO eDAO;
+    
+    public PagoServicio()
+    {
+        this.pagoDAO = new PagoDAO();
+        this.pDAO = new ParticipanteDAO();
+        this.eDAO = new EventoDAO();
+    }
+    
+    public boolean registrarPago(String correoParticipante, String codigoEvento, MetodoPago metodo, double monto) // se registra el pago por medio de la Id
+    {
+        Participante p = pDAO.buscarParticipante(correoParticipante);
+        Evento e = eDAO.buscarEvento(codigoEvento);
+        if (p == null || e == null)
+        {
+            System.err.println("Participante o evento no existe.");
+            return false;
+        }
+        return pagoDAO.registrarPago(new Pago(p, e, metodo, monto));
+    }
+    
+    public boolean registrarPago(Pago pago)
+    {
+        return pagoDAO.registrarPago(pago);
+    }
+    
+    public boolean existePago(String correoParticipante, String codigoEvento)
+    {
+        return pagoDAO.existePago(correoParticipante, codigoEvento);
+    }
+    
+    public List<Pago> listarPago()
+    {
+        return pagoDAO.listarPagos();
+    }
+    
+    public List<Pago> listarPagoPorEvento(String codigoEvento)
+    {
+        return pagoDAO.listarPagosPorEventos(codigoEvento);
+    }
+    
+    public List<Pago> listarPagoPorParticipante(String correoParticipante)
+    {
+        return pagoDAO.listarPagosPorParticipante(correoParticipante);
+    }
+    
+    public boolean eliminarPagoPorId(int id)
+    {
+        return pagoDAO.eliminarPagoPorId(id);
+    }
+}
