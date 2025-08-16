eb915d2cdda87097363c890ebba3e315f461bb78
maurelo860@gmail.com <maurelo860@gmail.com>
2025-08-15 18:31:57 -0600

    Se actualizas clases InscripcionDAO e InscripcionServicio para mejorar su funcionamiento

--- administracionEventos/src/main/java/com/mycompany/administracioneventos/servicios/InscripcionServicio.java
+++ administracionEventos/src/main/java/com/mycompany/administracioneventos/servicios/InscripcionServicio.java
@@ -4,20 +4,38 @@
  */
 package com.mycompany.administracioneventos.servicios;
 
+import com.mycompany.administracioneventos.dao.EventoDAO;
 import com.mycompany.administracioneventos.dao.InscripcionDAO;
-import com.mycompany.administracioneventos.modelos.Inscripcion;
+import com.mycompany.administracioneventos.dao.ParticipanteDAO;
+import com.mycompany.administracioneventos.modelos.*;
 import java.util.List;
 
 public class InscripcionServicio 
 {
     private final InscripcionDAO dao;
+    private final ParticipanteDAO participanteDAO;
+    private final EventoDAO eventoDAO;
     
     public InscripcionServicio()
     {
         this.dao = new InscripcionDAO();
+        this.participanteDAO = new ParticipanteDAO();
+        this.eventoDAO = new EventoDAO();
     }
     
-    public boolean registrarInscripcion(Inscripcion inscripcion) 
+    public boolean registrarInscripcionConstruccion(String correoParticipante, String codigoEvento, TipoInscripcion tipo) // Construye y registra la inscripcion  
+    {
+        Participante p = participanteDAO.buscarParticipante(correoParticipante);
+        Evento e = eventoDAO.buscarEvento(codigoEvento);
+        if (p == null || e == null)
+        {
+            System.err.println("Participante o evento no existen.");
+            return false;
+        }
+        return dao.agregarInscripcion(new Inscripcion(p, e, tipo));
+    }
+    
+    public boolean registrarInscripcion(Inscripcion inscripcion) // Registra la inscripcion ya contruida 
     {
         return dao.agregarInscripcion(inscripcion);
     }
